0b2d633a36fde7376f68062ae5ee0cfe3a9ec51b
Dean Pisani <dpisani93@gmail.com>
2015-12-05 00:01:11 +1100

    Add board setting to uploader. Add validation of settings

--- lib/lyrebird/uploader.rb
+++ lib/lyrebird/uploader.rb
@@ -1,7 +1,7 @@
 module Lyrebird
   class Uploader
 
-    attr_writer :current_tool
+    attr_writer :current_tool, :board
 
     def current_tool
       @current_tool || default_tool
@@ -9,6 +9,24 @@ module Lyrebird
 
     def default_tool
       :arduino
+    end
+
+    def board
+      @board || default_board
+    end
+
+    def default_board
+      :uno
+    end
+
+    def program_sketch(path, port)
+      throw ArgumentError unless valid_configuration?
+    end
+
+    private
+
+    def valid_configuration?
+      current_tool == :arduino and board == :uno
     end
   end
 end
--- spec/software/uploader_spec.rb
+++ spec/software/uploader_spec.rb
@@ -19,4 +19,35 @@ describe Lyrebird::Uploader do
       expect(@uploader.current_tool).to be :arduino
     end
   end
+
+  describe '#board' do
+    it 'is accessible' do
+      expect(@uploader.respond_to? :board).to be true
+    end
+
+    it 'can be set' do
+      @uploader.board = :plywood
+      expect(@uploader.board).to be :plywood
+    end
+
+    it 'defaults to UNO' do
+      expect(@uploader.board).to be :uno
+    end
+  end
+
+  describe '#program_sketch' do
+    context 'when current tool is not recognised' do
+      it 'throws an exception' do
+        @uploader.current_tool = :hammer
+        expect{@uploader.program_sketch('/dev/ttyAMC0', 'sketches/blink.ino')}.to raise_error ArgumentError
+      end
+    end
+
+    context 'when board is not recognised' do
+      it 'throws an exception' do
+        @uploader.board = :cheese
+        expect{@uploader.program_sketch('/dev/ttyAMC0', 'sketches/blink.ino')}.to raise_error ArgumentError
+      end
+    end
+  end
 end
